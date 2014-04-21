389ab8583f7c1d0a62237258f87efaa305a41927
Dmitry Momot <mail@dmomot.com>
2014-04-21 05:51:52 +0300

    loading config DB from file

---
+++ app/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+	'default' => array(
+		'connection_string' 			=> 'mysql:host=localhost;dbname=slim_test',
+		'username' 						=> 'slim_test',
+		'password' 						=> 'slim_test',
+		// 'id_column' 					=> 'id',
+		// 'id_column_overrides' 			=> array(),
+		// 'error_mode' 					=> \PDO::ERRMODE_EXCEPTION,
+		// 'driver_options' 				=> null,
+		// 'identifier_quote_character' 	=> null, // if this is null, will be autodetected
+		// 'limit_clause_style' 			=> null, // if this is null, will be autodetected
+		// 'logging' 						=> false,
+		// 'logger' 						=> null,
+		// 'caching'						=> false,
+		// 'return_result_sets' 			=> false,
+	),
+);
